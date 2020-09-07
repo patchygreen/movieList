478f7b89e9ed1caf68bc3ef7fc43286bc8c7c13d
patchygreen <pocrean@gmail.com>
2020-09-07 23:10:17 +0100

    add components and containers folder structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("movieList");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './containers/List';
+
+const App = () => {
+    return <List />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
