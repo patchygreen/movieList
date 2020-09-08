69fbfe91277b451fc7c870a76d29ce6af2259e73
patchygreen <pocrean@gmail.com>
2020-09-08 10:10:10 +0100

    added data.json fetch and async/await plus css styling with bootstrap

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Card = ({ movie }) => {
     return (
-        <div>
-            <h2></h2>
+        <div className='card'>
+            <img src={movie.img.src} alt={movie.img.alt} width='200' className='card-img-top'/>
+            <div className='card-body'>
+                <h2 className='card-title'>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+            </div>
+            <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>{`Distributor: ${movie.distributor}`}</li>
+                <li className='list-group-item'>{`Amount: ${movie.amount}`}</li>
+            </ul>
         </div>
     )
 }
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 import Card from '../components/Card/Card';
 
+
 class List extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true
+        };
+    }
+
+    async componentDidMount() {
+        const movies = await fetch('/src/assets/data.json');
+        const moviesJSON = await movies.json();
+
+        if (moviesJSON) {
+            this.setState({
+                data: moviesJSON,
+                loading: false
+            });
+        }
+
+        console.log(moviesJSON)
+    }
+
     render() {
-        return <Card />
+        const { data, loading } = this.state;
+
+        if (loading) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <div class='row'>
+                {data.map(movie => 
+                    <div class='col-sm-2'>
+                        <Card key={ movie.id } movie={ movie } />
+                    </div>
+                )}
+            </div>
+        );
     }
 }
 
